6acd112be5d2a074440d2df9fe394d152aba50c1
RAY Ray <159521800+Rayz121@users.noreply.github.com>
2024-12-21 15:56:08 +0800

    Add files via upload

---
+++ SignUpApp/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace SignUpApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
